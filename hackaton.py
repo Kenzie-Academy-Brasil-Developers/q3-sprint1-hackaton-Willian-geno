978fb12ba2b58d8712df65fc43cd20b2bcb23e38
Willian <willian@pop-os.localdomain>
2022-02-18 14:34:50 -0300

    ajustes no return

--- hackaton.py
+++ hackaton.py
@@ -6,12 +6,7 @@ def get_score(team_name, teams):
     index = 0;
     for item in teams:
         if item == team_name:
-            return print(f'A {team_name} ficou classificada em {index+1}° lugar!')
+            return (f'A {team_name} ficou classificada em {index+1}')
         index +=1
-    return print(f'A time "{team_name}" não é um time da lista!')
+    return (f'A time "{team_name}" não é um time da lista!')
 
-
-get_score( 'Team Liqued', hackathon_1)
-get_score("Team Ateliware", hackathon_1)
-get_score("Team Kenzie", hackathon_1)
-get_score("Team Mirum", hackathon_3)
